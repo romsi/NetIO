ad45c37c51370d6bb40a5023cf82584248ab5228
Romain Asanr <romain.asnar@gmail.com>
2011-12-27 10:43:40 +0100

    Add a type definition of sockaddr_in4_type.

--- src/socket_types.hpp
+++ src/socket_types.hpp
@@ -1,17 +1,21 @@
 #ifndef _SOCKET_TYPES_HPP_
 # define _SOCKET_TYPES_HPP_
 
-namespace NetIO {
+namespace netio {
+namespace ip {
 namespace detail {
 namespace socket_ops {
 
-typedef int socket_type;
+	typedef int socket_type;
 
-const socket_type invalid_socket = -1;
-const int socket_error = -1;
+	const socket_type invalid_socket = -1;
+	const int socket_error = -1;
+
+	typedef struct sockaddr_in sockaddr_in4_type;
 
 } // !socket_ops
 } // !detail
+} // !dip
 } // !NetIO
 
 #endif /* !_SOCKET_TYPES_HPP_ */
