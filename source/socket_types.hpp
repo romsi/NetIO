46eb123210c3afdaaa1001a00a9209a2bcb2dee9
Romsi <romsi@romsi-Ubuntu.(none)>
2011-12-27 23:25:07 +0100

    Change the namespace.

--- source/socket_types.hpp
+++ source/socket_types.hpp
@@ -2,9 +2,7 @@
 # define _SOCKET_TYPES_HPP_
 
 namespace netio {
-namespace ip {
 namespace detail {
-namespace socket_ops {
 
 	typedef int socket_type;
 
@@ -13,9 +11,7 @@ namespace socket_ops {
 
 	typedef struct sockaddr_in sockaddr_in4_type;
 
-} // !socket_ops
 } // !detail
-} // !dip
 } // !NetIO
 
 #endif /* !_SOCKET_TYPES_HPP_ */
