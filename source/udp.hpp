af4a054b45a1e0068e959cc9deb79f87ed7e4f2a
Romsi <romsi@romsi-Ubuntu.(none)>
2011-12-28 01:24:03 +0100

    Update the struct udp in class and add methods to get family, type and protocol of the protocol udp.

--- source/udp.hpp
+++ source/udp.hpp
@@ -5,16 +5,47 @@
 # include <netinet/in.h>
 
 # include "ip.hpp"
+# include "endpoint.hpp"
+# include "basic_datagram_socket.hpp"
 
 namespace netio {
 namespace ip {
 
-	struct udp {
+	class udp
+	{
+		/// Typedef
+	public:
+		typedef basic_datagram_socket<udp> socket;
+		typedef detail::endpoint<udp> endpoint;
 
+		/// Methods.
+	public:
+		static udp v4()
+		{
+			return udp(ip::v4);
+		}
+		// Obtain an identifier for the family of the protocol.
+		int family() const
+		{
+			return _family;
+		}
 		// Obtain an identifier for the type of the protocol.
-		static const int type = SOCK_DGRAM;
+		int type() const
+		{
+			return SOCK_DGRAM;
+		}
 		// Obtain an identifier for the protocol.
-  		static const int protocol = IPPROTO_UDP;
+		int protocol() const
+		{
+			return IPPROTO_UDP;
+		}
+	private:
+		explicit udp(int protocol_family) : _family(protocol_family)
+		{;}
+
+		/// Attributs.
+  	private:
+  		int _family;
 
 	}; // !udp
 
