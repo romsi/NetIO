11283165c7e47848964f6d921234a1b6cd96c5df
Romain Asnar <romain.asnar@gmail.com>
2011-12-29 13:32:35 +0100

    Add datagram socket service to perform synchronous operation and call the reactive socket service to start operation asynchronous.

---
+++ source/detail/datagram_socket_service.hpp
@@ -0,0 +1,112 @@
+#ifndef _DATAGRAM_SOCKET_SERVICE_HPP_
+# define _DATAGRAM_SOCKET_SERVICE_HPP_
+
+# include "socket_types.hpp"
+# include "reactor_op.hpp"
+# include "reactive_socket_service.hpp"
+# include "reactive_socket_recvfrom_op.hpp"
+
+# include "../io_service.hpp"
+
+namespace netio {
+namespace detail {
+
+	template<typename Protocol>
+	class datagram_socket_service
+		: public reactive_socket_service
+	{
+
+	protected:
+		typedef typename Protocol::endpoint endpoint_type;
+
+		/// Methods.
+	public:
+		datagram_socket_service(netio::io_service& io_service)
+			: reactive_socket_service(io_service)
+		{;}
+		//
+		bool open(const Protocol& protocol)
+		{
+			_socket = socket_ops::socket(
+				protocol.family(),
+				protocol.type(),
+				protocol.protocol()
+			);
+			return _socket != invalid_socket;
+		}
+		bool is_open() const
+		{
+			return _socket != invalid_socket;
+		}
+		//
+		bool bind(endpoint_type& endpoint)
+		{
+			return socket_ops::bind(
+				_socket,
+				reinterpret_cast<struct sockaddr*>(endpoint.data()),
+				endpoint.size());
+		}
+		//
+		bool close()
+		{
+			if (!is_open())
+				return (true);
+			bool success = socket_ops::close(_socket);
+			_socket = invalid_socket;
+			return success;
+		}
+		//
+		ssize_t recvfrom(
+			endpoint_type& from,
+			void* buffer,
+			size_t len
+		)
+		{
+			socklen_t size = from.size();
+			return detail::socket_ops::sync_recvfrom(
+				_socket,
+				buffer,
+				len,
+				0,
+				reinterpret_cast<struct sockaddr*>(from.data()),
+				&size);
+		}
+		//
+		ssize_t sendto(
+			endpoint_type& to,
+			void* buffer,
+			size_t len
+		)
+		{
+			return detail::socket_ops::sync_sendto(
+				_socket,
+				buffer,
+				len,
+				0,
+				reinterpret_cast<struct sockaddr*>(to.data()),
+				to.size());
+		}
+		//
+		template<typename ReadHandler>
+		void async_recvfrom(
+			endpoint_type& from,
+			void* buffer,
+			size_t& len,
+			ReadHandler handler
+		)
+		{
+			reactor_op* op = new reactive_socket_recvfrom_op<endpoint_type, ReadHandler>(_socket, from, buffer, len, handler);
+			// start operation.
+			this->start_op(operations::read, _socket, op);
+		}
+
+		/// Attributs.
+	private:
+		socket_type _socket;
+		
+	};
+
+} // !detail
+} // !netio
+
+#endif /* !_DATAGRAM_SOCKET_SERVICE_HPP_ */
