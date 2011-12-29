ec67f299736ceac8f0041d162636d44b89fbc72a
Romain Asnar <romain.asnar@gmail.com>
2011-12-29 13:26:54 +0100

    Create a reactor operation to wrap the data of operation.

---
+++ source/detail/reactor_op.hpp
@@ -0,0 +1,47 @@
+#ifndef _REACTOR_OPERATION_HPP_
+# define _REACTOR_OPERATION_HPP_
+
+namespace netio {
+namespace detail {
+
+	class reactor_op
+	{
+
+	protected:
+		typedef bool (*perform_func_type)(reactor_op*);
+		typedef void (*complete_func_type)(reactor_op*, size_t);
+
+		/// Methods.
+	public:
+		// Perform the operation. Returns true if it is finished.
+		bool perform()
+		{
+			return _perform_func(this);
+		}
+		void complete(size_t bytes_transferred)
+		{
+			_complete_func(this, bytes_transferred);
+		}
+
+	protected:
+		reactor_op(perform_func_type perform_func, complete_func_type complete_func)
+			: _bytes_transferred(0),
+		    _perform_func(perform_func),
+		    _complete_func(complete_func)
+		{;}
+
+		/// Attributs.
+	public:
+		// The number of bytes transferred, to be passed to the completion handler.
+		size_t _bytes_transferred;
+
+	private:
+		perform_func_type _perform_func;
+		complete_func_type _complete_func;
+
+	}; // !reactor_op
+
+} // !detail
+} // !netio
+
+#endif /* !_REACTOR_OPERATION_HPP_ */
