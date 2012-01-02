ce86ecf14ff45993eca31a483d6b9e28dbb74221
Romain Asnar <romain.asnar@gmail.com>
2012-01-02 11:20:23 +0100

    Add task implementation and delete the method poll.

--- source/io_service.hpp
+++ source/io_service.hpp
@@ -1,9 +1,14 @@
 #ifndef _IO_SERVICE_HPP_
 # define _IO_SERVICE_HPP_
 
+// # include "multiplexer.hpp"
+// multiplexer* _multiplexer;
+
 namespace netio {
 
 // Pour enregistrer le reactor, penser au fonction friend.
+
+	typedef detail::task_io_service task_service_type;
 
 	class io_service
 	{
@@ -12,13 +17,25 @@ namespace netio {
 	public:
 		io_service()
 		{;}
-		bool run();
-		bool poll();
-		bool stop();
+		/*
+		**
+		** @return The number of handlers that were executed.
+		*/
+		size_t run()
+		{
+			return _task.run();
+		}
+		/*
+		**
+		*/
+		bool stop()
+		{
+			_task.stop();
+		}
 
 		/// Attributs.
 	private:
-		// reactor* _reactor;
+		task_service_type _task;
 
 	}; // !io_service
 
