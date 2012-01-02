7b996733924df2577b983d348016860ff86daa81
Romain Asnar <romain.asnar@gmail.com>
2012-01-02 11:21:11 +0100

    Add definition of multiplexer and operations type.

---
+++ source/detail/multiplexer.hpp
@@ -0,0 +1,22 @@
+#ifndef _MULTIPLEXER_HPP_
+# define _MULTIPLEXER_HPP_
+
+namespace netio {
+namespace detail {
+
+	namespace operations {
+		
+		enum operation_type
+		{
+			write = 1,
+			read
+		};
+
+	} // ! operations
+
+	typedef select_reactor multiplexer;
+
+} // !detail
+} // !netio
+
+#endif /* !_MULTIPLEXER_HPP_ */
