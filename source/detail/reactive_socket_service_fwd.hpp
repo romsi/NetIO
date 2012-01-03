ff0a3410e020115bdcc1584d2810e44f79e853e1
Romain Asnar <romain.asnar@gmail.com>
2012-01-03 11:58:28 +0100

    Create a forward declaration of reactive socket service class.

---
+++ source/detail/reactive_socket_service_fwd.hpp
@@ -0,0 +1,12 @@
+#ifndef _REACTIVE_SOCKET_SERVICE_FWD_HPP_
+# define _REACTIVE_SOCKET_SERVICE_FWD_HPP_
+
+namespace netio {
+namespace detail {
+
+	class reactive_socket_service;
+
+} // !detail
+} // !netio
+
+#endif /* !_REACTIVE_SOCKET_SERVICE_FWD_HPP_ */
