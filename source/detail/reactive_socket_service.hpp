4b335940021e73b5b53b07f965a49e60023208ff
Romain Asnar <romain.asnar@gmail.com>
2012-01-08 12:48:10 +0100

    Change the name of the method.

--- source/detail/reactive_socket_service.hpp
+++ source/detail/reactive_socket_service.hpp
@@ -2,6 +2,7 @@
 # define _REACTIVE_SOCKET_SERVICE_HPP_
 
 # include "socket_types.hpp"
+# include "operation_type.hpp"
 # include "reactor_op.hpp"
 
 # include "../io_service.hpp"
@@ -12,15 +13,17 @@ namespace detail {
 	class reactive_socket_service
 	{
 
+		typedef detail::operations::operation_type operation_type;
+
 		/// Methods.
 	public:
 		reactive_socket_service(netio::io_service& io_service)
 			: _service(io_service)
 		{;}
 		//
-		bool start_op(operations::operation_type type, reactor_op* op)
+		bool start_op(operation_type type, socket_type socket, reactor_op* op)
 		{
-			return _service.register_task(type, op);
+			return _service.start_op(type, socket, op);
 		}
 
 		/// Attributs.
