91012eaf4a82bb940821b855118aa67ee8ff8f1f
Romain Asnar <romain.asnar@gmail.com>
2012-01-09 11:58:51 +0100

    Minimal update.

--- source/detail/task_io_service.hpp
+++ source/detail/task_io_service.hpp
@@ -1,8 +1,8 @@
 #ifndef _TASK_IO_SERVICE_HPP_
 # define _TASK_IO_SERVICE_HPP_
 
-# include <queue>
-
+# include "socket_types.hpp"
+# include "operation_type.hpp"
 # include "reactor_op.hpp"
 # include "multiplexer.hpp"
 
@@ -12,34 +12,35 @@ namespace netio {
 
 	namespace detail {
 
-	// Pour enregistrer le reactor, penser au fonction friend.
-
 		class task_io_service
 		{
+
+			typedef detail::operations::operation_type operation_type;
 
 			/// Methods.
 		public:
 			task_io_service()
-				// : _multiplexer(0)
 			{;}
 			//
 			size_t run()
 			{
-				// return _multiplexer.run(_op_queue);
+				return _multiplexer.run();
 			}
+			//
 			bool stop()
 			{
-				;
+				// return _multiplexer.stop();
 			}
-			bool register_task(operations::operation_type type, reactor_op* op)
+			//
+			bool register_task(operation_type type, socket_type socket, reactor_op* op)
 			{
-				;
+				return _multiplexer.start_op(type, socket, op);
 			}
 
 			/// Attributs.
 		private:
-			//multiplexer* _multiplexer;
-			std::queue<reactor_op> _op_queue;
+			multiplexer _multiplexer;
+			
 
 		}; // !task_io_service
 
