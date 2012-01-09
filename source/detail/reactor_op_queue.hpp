eccc12714b6b88e4317b125fb00983710878cfbc
Romain Asnar <romain.asnar@gmail.com>
2012-01-09 12:03:32 +0100

    Create class reactor operation queue to enqueue operation and socket into the map.

---
+++ source/detail/reactor_op_queue.hpp
@@ -0,0 +1,53 @@
+#ifndef _REACTOR_OP_QUEUE_HPP_
+# define _REACTOR_OP_QUEUE_HPP_
+
+# include <iostream>
+
+# include <queue>
+# include <map>
+
+# include "reactor_op.hpp"
+
+namespace netio {
+namespace detail {
+
+	class reactor_op_queue
+	{
+
+		typedef std::queue<reactor_op*> op_queue_type;
+		typedef std::map<socket_type, op_queue_type*> op_map_type;
+
+
+		/// Methods.
+	public:
+		reactor_op_queue()
+		{;}
+		//
+		bool enqueue(socket_type socket, reactor_op* op)
+		{
+			std::pair<op_map_type::iterator, bool> ret = _op_map.insert(op_map_type::value_type(socket, new op_queue_type));
+			ret.first->second->push(op);
+			return true;
+		}
+		//
+		std::queue<socket_type> get_descriptors()
+		{
+			std::queue<socket_type> descriptors;
+			op_map_type::iterator i;
+			for (i = _op_map.begin(); i != _op_map.end(); i++)
+    			descriptors.push((*i).first);
+			return descriptors;
+		}
+		//
+		bool perform_operation(socket_type socket, reactor_op* op);
+		
+		/// Attributs.
+	private:
+		op_map_type _op_map;
+
+	}; // !reactor_op_queue
+
+} // !detail
+} // !netio
+
+#endif /* _REACTOR_OP_QUEUE_HPP_ */
