8df59d7039372fcaabb282ff1f29c767f3148fbe
Romain Asnar <romain.asnar@gmail.com>
2012-01-09 11:58:34 +0100

    Minimal update.

--- source/io_service.hpp
+++ source/io_service.hpp
@@ -1,10 +1,12 @@
 #ifndef _IO_SERVICE_HPP_
 # define _IO_SERVICE_HPP_
 
- # include "detail/task_io_service.hpp"
- # include "detail/reactive_socket_service_fwd.hpp"
- # include "detail/reactor_op.hpp"
- # include "detail/multiplexer.hpp"
+# include "detail/socket_types.hpp"
+# include "detail/task_io_service.hpp"
+# include "detail/reactive_socket_service_fwd.hpp"
+# include "detail/operation_type.hpp"
+# include "detail/reactor_op.hpp"
+# include "detail/multiplexer.hpp"
 
 namespace netio {
 
@@ -12,7 +14,7 @@ namespace netio {
 
 	class io_service
 	{
-		friend class detail::reactive_socket_service;
+		//friend class detail::reactive_socket_service;
 
 		typedef detail::operations::operation_type operation_type;
 		typedef detail::reactor_op reactor_op;
@@ -37,10 +39,9 @@ namespace netio {
 			return _task.stop();
 		}
 
-	private:
-		bool register_task(operation_type type, reactor_op* op)
+		bool start_op(operation_type type, detail::socket_type socket, reactor_op* op)
 		{
-			return _task.register_task(type, op);
+			return _task.register_task(type, socket, op);
 		}
 
 		/// Attributs.
