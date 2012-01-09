1b0ec7b62ccc577d55a24c057290a5b0de9ddc51
Romain Asnar <romain.asnar@gmail.com>
2012-01-09 12:01:32 +0100

    Add async operation in server.

--- source/main.cpp
+++ source/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 
+#include <map>
+#include <queue>
+
 #include "ip/udp.hpp"
 
 void client(netio::io_service& io_service)
@@ -23,10 +26,17 @@ void server(netio::io_service& io_service)
 	netio::ip::udp::socket socket(io_service, endpoint);
 	netio::ip::udp::endpoint from;
 	char buffer[1024];
-	if (socket.async_recvfrom(from, buffer, sizeof(buffer), &recvfromHandler) > 0)
+	/*
+	** SYNC
+	if (socket.recvfrom(from, buffer, sizeof(buffer)) > 0)
 		std::cout << "MESSAGE : " << buffer << std::endl;
 	else
 		std::cout << "ERROR RECVFROM !" << std::endl;
+	*/
+	/*
+	** ASYNC
+	*/
+	socket.async_recvfrom(from, buffer, sizeof(buffer), &recvfromHandler);
 }
 
 int main()
@@ -34,5 +44,6 @@ int main()
 	netio::io_service io_service;
 	std::cout << "Start" << std::endl;
 	server(io_service);
+	io_service.run();
 	std::cout << "End" << std::endl;
 }
