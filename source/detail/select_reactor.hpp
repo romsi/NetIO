fffe572e60571e8f1252c0431ae8ccc3a91dfedb
Romain Asanr <romain.asnar@gmail.com>
2012-01-11 01:35:25 +0100

    Create isset method in fdset wrapper to know if the file descriptor is set in fd_set structure. Create perform operation method to perform operation who the file descriptor is set in fd_set structure. Call the perform operation and return 0 if hasn't work, in run method.

--- source/detail/select_reactor.hpp
+++ source/detail/select_reactor.hpp
@@ -21,35 +21,6 @@ namespace detail {
 
 		typedef detail::operations::operation_type operation_type;
 
-		/// Methods.
-	public:
-		select_reactor()
-		{;}
-		bool start_op(operation_type type, socket_type socket, reactor_op* op)
-		{
-			return _op_queue[type].enqueue(socket, op);
-		}
-		//
-		bool register_descriptor();
-		//
-		size_t run()
-		{
-			socket_type max_readfd = _readfds.set_all(_op_queue[operations::read].get_descriptors());
-			socket_type max_writefd = _writefds.set_all(_op_queue[operations::write].get_descriptors());
-			socket_type max_fd = (max_readfd > max_writefd) ? max_readfd : max_writefd;
-			std::cout << "Max fd: " << max_fd << std::endl;
-			_op_queue[operations::read].perform_operation(max_fd);
-			ssize_t ready = 0;
-			if ((ready = ::select(max_fd + 1, &(_readfds.data), &(_writefds.data), 0, 0)) == -1)
-				perror("select");
-			std::cout << "Descriptor ready: " << ready << std::endl;
-			return (ready == -1) ? 0 : ready;
-		}
-
-		/// Attributs.
-	private:
-		reactor_op_queue _op_queue[operations::max_ops];
-
 		template<typename Descriptor>
 		struct fdset
 		{
@@ -79,11 +50,67 @@ namespace detail {
 				std::cout << "FD: " << fd << std::endl;
 				FD_SET(fd, &data);
 			}
+			//
+			bool isset(Descriptor& fd)
+			{
+				return FD_ISSET(fd, &data);
+			}
 
 			/// Attributs.
 			fd_set data;
 
-		};
+		}; // !fdset
+
+		/// Methods.
+	public:
+		select_reactor()
+		{;}
+		bool start_op(operation_type type, socket_type& socket, reactor_op* op)
+		{
+			return _op_queue[type].enqueue(socket, op);
+		}
+		//
+		bool register_descriptor();
+		//
+		size_t run()
+		{
+			socket_type max_readfd = _readfds.set_all(_op_queue[operations::read].get_descriptors());
+			socket_type max_writefd = _writefds.set_all(_op_queue[operations::write].get_descriptors());
+			socket_type max_fd = (max_readfd > max_writefd) ? max_readfd : max_writefd;
+			// std::cout << "Max fd: " << max_fd << std::endl;
+			bool has_work = !(max_fd == invalid_socket);
+			if (!has_work)
+			{
+				std::cout << "No work" << std::endl;
+				return 0;
+			}
+			std::cout << "Start select !" << std::endl;
+			ssize_t ready = 0;
+			if ((ready = ::select(max_fd + 1, &(_readfds.data), &(_writefds.data), 0, 0)) == -1)
+				perror("select");
+			std::cout << "Ending select !" << std::endl;
+			has_work = !(ready == -1);
+			if (!has_work)
+				return 0;
+			perform_operation(_readfds);
+			perform_operation(_writefds);
+			return ready;
+		}
+	private:
+		void perform_operation(fdset<socket_type>& data)
+		{
+			std::queue<socket_type> descriptors = _op_queue[operations::read].get_descriptors();
+			while (!descriptors.empty())
+			{
+				if (data.isset(descriptors.front()))
+					_op_queue[operations::read].perform_operation(descriptors.front());
+				descriptors.pop();
+			}
+		}
+
+		/// Attributs.
+	private:
+		reactor_op_queue _op_queue[operations::max_ops];
 
 		fdset<socket_type> _readfds;
 		fdset<socket_type> _writefds;
@@ -93,4 +120,4 @@ namespace detail {
 } // !detail
 } // !netio
 
-#endif /* _SELECT_REACTOR_HPP_ */
+#endif /* !_SELECT_REACTOR_HPP_ */
