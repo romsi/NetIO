b055b95e35b362be58bd32838bce7ce6bb05c4c2
Romain Asanr <romain.asnar@gmail.com>
2012-01-12 15:23:56 +0100

    Delete poll function. Check errno for non blocking sendto and recvfrom operation.

--- source/detail/impl/socket_ops.cpp
+++ source/detail/impl/socket_ops.cpp
@@ -58,11 +58,8 @@ namespace socket_ops {
 	)
 	{
 		ssize_t bytes = ::recvfrom(sockfd, buffer, len, flags, addr, addrlen);
-		if (bytes == -1) {
+		if (bytes == -1)
 			perror("recvfrom");
-			
-		}
-			
 		return (bytes);
 	}
 
@@ -81,9 +78,6 @@ namespace socket_ops {
 			bytes = socket_ops::recvfrom(sockfd, buffer, len, flags, addr, addrlen);
 			if (bytes >= 0)
 				return (bytes);
-			std::cout << "POLL READ" << std::endl;
-			if (!socket_ops::poll_read(sockfd))
-				return (0);
 		}
 	}
 
@@ -101,6 +95,13 @@ namespace socket_ops {
 		for (;;)
 		{
 			bytes = socket_ops::recvfrom(sockfd, buffer, len, flags, addr, addrlen);
+			if (errno == EAGAIN || errno == EWOULDBLOCK)
+			{
+				std::cout << "Recvfrom EGAIN operation." << std::endl;
+				return false;	
+			}
+			if (errno == EINTR)
+				continue ;
 			if (bytes >= 0)
 			{
 				bytes_transferred = bytes;
@@ -122,7 +123,7 @@ namespace socket_ops {
 	{
 		ssize_t bytes = ::sendto(sockfd, buffer, len, flags, addr, addrlen);
 		if (!(bytes >= 0))
-			std::cout << "ERROR detail::sendto" << std::endl;
+			perror("sendto");
 		return (bytes);
 	}
 
@@ -141,48 +142,38 @@ namespace socket_ops {
 			bytes = socket_ops::sendto(sockfd, buffer, len, flags, addr, addrlen);
 			if (bytes >= 0)
 				return (bytes);
-			std::cout << "POLL WRITE" << std::endl;
-			if (!socket_ops::poll_write(sockfd))
-				return (0);
 		}
 	}
 
-	int poll(
-		struct pollfd *fds,
-		nfds_t nfds,
-		int timeout
+	ssize_t non_blocking_sendto(
+		detail::socket_type sockfd,
+		void *buffer,
+		size_t len,
+		int flags,
+		struct sockaddr *addr,
+		socklen_t addrlen,
+		size_t& bytes_transferred
 	)
 	{
-		int returns = ::poll(fds, nfds, timeout);
-		return (returns);
-	}
-
-	bool poll_read(detail::socket_type sockfd)
-	{
-		pollfd fds;
-		fds.fd = sockfd;
-		fds.events = POLLIN;
-		fds.revents = 0;
-		int result = socket_ops::poll(&fds, 1, -1);
-		if (result == 0)
-			return (false);
-		else if (result == socket_error)
-			return (false);
-		return (true);
-	}
-
-	bool poll_write(detail::socket_type sockfd)
-	{
-		pollfd fds;
-		fds.fd = sockfd;
-		fds.events = POLLOUT;
-		fds.revents = 0;
-		int result = socket_ops::poll(&fds, 1, -1);
-		if (result == 0)
-			return (false);
-		else if (result == socket_error)
-			return (false);
-		return (true);
+		ssize_t bytes = 0;
+		for (;;)
+		{
+			bytes = socket_ops::sendto(sockfd, buffer, len, flags, addr, addrlen);
+			if (errno == EAGAIN || errno == EWOULDBLOCK)
+			{
+				std::cout << "Sendto EGAIN operation." << std::endl;
+				return false;	
+			}
+			if (errno == EINTR)
+				continue ;
+			if (bytes >= 0)
+			{
+				bytes_transferred = bytes;
+				return true;
+			}
+			bytes_transferred = 0;
+			return true;
+		}
 	}
 
 } // !socket_ops
